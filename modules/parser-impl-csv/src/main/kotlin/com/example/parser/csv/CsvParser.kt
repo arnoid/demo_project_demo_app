d757c3cfc3bc0d58a186edeb0de08f13b7bbf8c2
arnoid <stratoangel@gmail.com>
2019-12-04 14:41:34 +0100

    [FIX] merged processor logic

--- modules/parser-impl-csv/src/main/kotlin/com/example/parser/csv/CsvParser.kt
+++ modules/parser-impl-csv/src/main/kotlin/com/example/parser/csv/CsvParser.kt
@@ -16,11 +16,53 @@ class CsvParser(
         val result = mutableListOf<List<String>>()
 
         var endOfFile = false
-        val onEofCallback = { endOfFile = true }
 
         FileLoop@ while (true) {
 
-            val row = parseRow(input, onEofCallback)
+            val row = mutableListOf<String>()
+            val cellBuilder = StringBuilder()
+
+            var isInContainer = false
+            var lastChar = ' '//anything but " (double quotes) char
+
+            RowLoop@ while (true) {
+                val next = input.read()
+                val nextChar = next.toChar()
+
+                if (next == EOF) {
+                    endOfFile = true
+                }
+
+                when {
+                    !isInContainer && (nextChar == DELIMITER_ROW || next == EOF) -> {
+                        //flush cell
+                        row.add(cellBuilder.toString())
+
+                        break@RowLoop
+                    }
+                    nextChar == cellDelimiter -> {
+                        row.add(cellBuilder.toString())
+                        cellBuilder.clear()
+                    }
+                    !isInContainer && nextChar == QUOTE && lastChar == QUOTE -> {
+                        //mimic for the double quote lookup
+                        //according to RFC4180 double quotes are allowed in quoted section ONLY
+                        isInContainer = true
+                        cellBuilder.append(QUOTE)
+                    }
+                    nextChar == QUOTE && isInContainer -> {
+                        isInContainer = false
+                    }
+                    nextChar == QUOTE -> {
+                        isInContainer = true
+                    }
+                    else -> {
+                        cellBuilder.append(nextChar)
+                    }
+                }
+
+                lastChar = nextChar
+            }
 
             result.add(row)
 
@@ -31,55 +73,6 @@ class CsvParser(
         }
 
         return result
-    }
-
-    private fun parseRow(input: Reader, onEofCallback: () -> Unit): MutableList<String> {
-        val row = mutableListOf<String>()
-        val cellBuilder = StringBuilder()
-
-        var isInContainer = false
-        var lastChar = ' '//anything but " (double quotes) char
-
-        RowLoop@ while (true) {
-            val next = input.read()
-            val nextChar = next.toChar()
-
-            if (next == EOF) {
-                onEofCallback()
-            }
-
-            when {
-                !isInContainer && (nextChar == DELIMITER_ROW || next == EOF) -> {
-                    //flush cell
-                    row.add(cellBuilder.toString())
-
-                    break@RowLoop
-                }
-                nextChar == cellDelimiter -> {
-                    row.add(cellBuilder.toString())
-                    cellBuilder.clear()
-                }
-                !isInContainer && nextChar == QUOTE && lastChar == QUOTE -> {
-                    //mimic for the double quote lookup
-                    //according to RFC4180 double quotes are allowed in quoted section ONLY
-                    isInContainer = true
-                    cellBuilder.append(QUOTE)
-                }
-                nextChar == QUOTE && isInContainer -> {
-                    isInContainer = false
-                }
-                nextChar == QUOTE -> {
-                    isInContainer = true
-                }
-                else -> {
-                    cellBuilder.append(nextChar)
-                }
-            }
-
-            lastChar = nextChar
-        }
-
-        return row
     }
 
     companion object {
